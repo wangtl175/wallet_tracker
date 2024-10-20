85e2ec6910a59542d338f6dd4f4272fc53d0b81c
wangtl175 <wangtlong3@mail2.sysu.edu.cn>
2024-10-20 16:39:17 +0800

    优化性能

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
+use std::sync::Arc;
 use alloy::eips::BlockId;
-use alloy::primitives::{address, Address, map::HashSet, TxHash};
+use alloy::primitives::{address, Address, map::AddressHashSet, TxHash};
 use anyhow::Result;
 use alloy::providers::{Provider, ProviderBuilder, WsConnect};
 use futures_util::StreamExt;
 use lettre::{AsyncSmtpTransport, AsyncTransport, Message, Tokio1Executor};
 use lettre::transport::smtp::authentication::Credentials;
+use tokio::sync::Mutex;
 
 const EMAIL_RECEIVER: &str = "email receiver";
 const EMAIL_SENDER: &str = "email sender";
@@ -34,28 +36,39 @@ async fn main() -> Result<()> {
     let rpc_url = "wss://eth-mainnet.g.alchemy.com/v2/LttRLGRc066mddQWggs0tttdq9FwmTZh";  // mainnet
     let ws = WsConnect::new(rpc_url);
     let provider = ProviderBuilder::new().on_ws(ws).await?;
-    let smart_moneys  = HashSet::from([address!("aCab087f7f0977c31d68E8BAe117069a90Dc6574")]);
+    let mut smart_moneys = AddressHashSet::default();
+    smart_moneys.insert(address!("aCab087f7f0977c31d68E8BAe117069a90Dc6574"));
+    let smart_moneys = Arc::new(smart_moneys);
 
     println!("connect to {rpc_url}");
 
     let sub = provider.subscribe_blocks().await?;
     let mut stream = sub.into_stream();
 
+    let provider = Arc::new(Mutex::new(provider));
+
     while let Some(block) = stream.next().await {
         if block.header.number % 100 == 0 {
             println!("Received block number: {}", block.header.number);
         }
 
-        let transactions = provider.get_block_receipts(BlockId::from(block.header.hash)).await?;
-        if let Some(transactions) = transactions {
-            for transaction in transactions {
-                if smart_moneys.contains(&transaction.from) {
-                    println!("smart money {} has a transaction {}", transaction.from, transaction.transaction_hash);
+        let provider = provider.clone();
+        let smart_moneys = smart_moneys.clone();
 
-                    send_email(transaction.from, transaction.transaction_hash).await;
+        tokio::spawn(async move {
+            let provider = provider.lock().await;
+
+            let transactions = provider.get_block_receipts(BlockId::from(block.header.hash)).await;
+            if let Ok(Some(transactions)) = transactions {
+                for transaction in transactions {
+                    if smart_moneys.contains(&transaction.from) {
+                        println!("smart money {} has a transaction {}", transaction.from, transaction.transaction_hash);
+
+                        send_email(transaction.from, transaction.transaction_hash).await;
+                    }
                 }
             }
-        }
+        });
     }
 
     Ok(())
